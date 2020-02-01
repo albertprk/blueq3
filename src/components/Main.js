7e7e88e4cd7d27c295eb7bb5bb69af6d59b3a7ae
Albert Park <albert.park@hotmail.com>
2020-01-31 23:31:28 -0800

    Added state to the main page including username and profilepic

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,28 @@
 import React, {Component} from "react";
 
 export class Main extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "Albert",
+            profilePic: "https://i.imgur.com/rWsf1oK.jpg"
+        }
+    }
+
+    handleClickOn = (e) => {
+
+    }
+
     render() {
         return (
             <div className="App">
                 <header className="App-header">
+                    <img src={this.state.profilePic} width="125px" height="125px" radius="70px" className="profilePic"></img>
+                    <div className="welcome">Welcome, {this.state.username}!</div>
+                    <br/><br/>
                     <form>
-                        <input type="submit" value="send help request" className="request"/>
+                        <input type="submit" value="blue shirt signal" className="request" onClick={this.handleClickOn}/>
                     </form>
                 </header>
             </div>
